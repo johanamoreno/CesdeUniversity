dbfadfb9a87b1f79e3f7cc3cf9da80145a53c559
johanamoreno <johana.moreno1118@gmail.com>
2020-12-12 21:32:06 -0500

    Initial commit

---
+++ DAL/UniversityContext.cs
@@ -0,0 +1,26 @@
+﻿using CesdeUniversity.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace CesdeUniversity.DAL
+{
+    public class UniversityContext : DbContext
+    {
+        public UniversityContext() : base("UniversityContext")
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
